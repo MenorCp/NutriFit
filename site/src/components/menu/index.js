8a82fd595a4c42e82810c1bac3937fd91cf1d75d
Eric <ets1603@gmail.com>
2022-06-19 23:58:41 -0300

    componentes adicionados

---
+++ site/src/components/menu/index.js
@@ -0,0 +1,47 @@
+import storage from 'local-storage'
+
+import { useNavigate } from 'react-router-dom';
+
+import './index.scss';
+
+import { Link } from 'react-router-dom';
+
+
+export default function Index() {
+
+    const navigate = useNavigate();
+
+    function sairClick() {
+        storage.remove('usuario-logado')
+        navigate('/login');
+    }
+
+    return (
+        <nav className="comp-menu">
+            <div>
+                <div className='logo'>
+                    <div className='img-planta'></div>
+                    <p className='nutrifit-text'>NUTRIFIT</p>
+                </div>
+
+                <div className='menu-items'>
+                    <Link to='../'>
+                        <div>Home</div>
+                    </Link>
+                    <Link to='../form'>
+                        <div>Cadastrar</div>
+                    </Link>
+                    <Link to='../adm'>
+                        <div>Consultar</div>
+                    </Link>
+                </div>
+            </div>
+
+            <div className='menu-items-sair'>
+                <div>
+                    <div onClick={sairClick} >Sair</div>
+                </div>
+            </div>
+        </nav>
+    )
+}
